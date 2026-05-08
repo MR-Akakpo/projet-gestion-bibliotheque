35c76687968c06e40672ea2daa781f70f2ecbad5
MR-Akakpo <ahmadouakakpo@gmail.com>
2026-05-08 21:57:15 +0000

    Mise à jour des fichiers

--- script.js
+++ script.js
@@ -1,161 +1,231 @@
 let books = [];
-let editIndex = -1;
 
-const tableBody = document.getElementById("bookTableBody");
+const booksBody = document.getElementById("booksBody");
+
+const searchInput = document.getElementById("searchInput");
+
 const form = document.getElementById("bookForm");
 
+const modal = document.getElementById("bookModal");
+
+const closeModal = document.querySelector(".close");
+
+let editIndex = -1;
+
+/* CHARGEMENT XML */
+
 fetch("books.xml")
-    .then(response => response.text())
-    .then(data => {
 
-        const parser = new DOMParser();
-        const xml = parser.parseFromString(data, "application/xml");
+.then(response => response.text())
 
-        const xmlBooks = xml.getElementsByTagName("book");
+.then(data => {
 
-        for (let book of xmlBooks) {
+    const parser = new DOMParser();
 
-            books.push({
-                title: book.getElementsByTagName("title")[0].textContent,
-                author: book.getElementsByTagName("author")[0].textContent,
-                year: book.getElementsByTagName("year")[0].textContent,
-                price: book.getElementsByTagName("price")[0].textContent,
-                image: book.getElementsByTagName("image")[0].textContent
-            });
+    const xml = parser.parseFromString(data, "text/xml");
 
-        }
+    const xmlBooks = xml.getElementsByTagName("book");
 
-        displayBooks();
+    books = [];
 
-    });
+    for(let book of xmlBooks){
 
-function displayBooks(filteredBooks = books) {
+        books.push({
 
-    tableBody.innerHTML = "";
+            title: book.getElementsByTagName("title")[0].textContent,
 
-    filteredBooks.forEach((book, index) => {
+            author: book.getElementsByTagName("author")[0].textContent,
 
-        tableBody.innerHTML += `
-            <tr>
+            year: book.getElementsByTagName("year")[0].textContent,
 
-                <td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.year}</td>
-                <td>${book.price} FCFA</td>
+            price: book.getElementsByTagName("price")[0].textContent,
 
-                <td>
+            image: book.getElementsByTagName("image")[0].textContent
 
-                    <button class="view-btn"
-                        onclick="viewBook(${index})">
+        });
+
+    }
+
+    displayBooks(books);
+
+});
+
+/* AFFICHAGE */
+
+function displayBooks(data){
+
+    booksBody.innerHTML = "";
+
+    data.forEach((book, index) => {
+
+        booksBody.innerHTML += `
+
+        <tr>
+
+            <td>${book.title}</td>
+
+            <td>${book.author}</td>
+
+            <td>${book.year}</td>
+
+            <td>${book.price} FCFA</td>
+
+            <td>
+
+                <div class="actions">
+
+                    <button class="view-btn" onclick="viewBook(${index})">
+
                         Voir
+
                     </button>
 
-                    <button class="edit-btn"
-                        onclick="editBook(${index})">
+                    <button class="edit-btn" onclick="editBook(${index})">
+
                         Modifier
+
                     </button>
 
-                    <button class="delete-btn"
-                        onclick="deleteBook(${index})">
+                    <button class="delete-btn" onclick="deleteBook(${index})">
+
                         Supprimer
+
                     </button>
 
-                </td>
+                </div>
 
-            </tr>
+            </td>
+
+        </tr>
+
         `;
 
     });
 
 }
 
-form.addEventListener("submit", function(e) {
+/* RECHERCHE */
 
-    e.preventDefault();
+searchInput.addEventListener("keyup", () => {
 
-    const book = {
-
-        title: title.value,
-        author: author.value,
-        year: year.value,
-        price: price.value,
-        image: image.value
-
-    };
-
-    if (editIndex === -1) {
-
-        books.push(book);
-
-    } else {
-
-        books[editIndex] = book;
-        editIndex = -1;
-
-    }
-
-    displayBooks();
-
-    form.reset();
-
-});
-
-function deleteBook(index) {
-
-    if (confirm("Voulez-vous supprimer ce livre ?")) {
-
-        books.splice(index, 1);
-
-        displayBooks();
-
-    }
-
-}
-
-function editBook(index) {
-
-    const book = books[index];
-
-    title.value = book.title;
-    author.value = book.author;
-    year.value = book.year;
-    price.value = book.price;
-    image.value = book.image;
-
-    editIndex = index;
-
-}
-
-function viewBook(index) {
-
-    const book = books[index];
-
-    document.getElementById("modal").style.display = "block";
-
-    modalImage.src = book.image;
-    modalTitle.textContent = book.title;
-    modalAuthor.textContent = "Auteur : " + book.author;
-    modalYear.textContent = "Année : " + book.year;
-    modalPrice.textContent = "Prix : " + book.price + " FCFA";
-
-}
-
-document.querySelector(".close")
-.addEventListener("click", () => {
-
-    document.getElementById("modal").style.display = "none";
-
-});
-
-document.getElementById("searchInput")
-.addEventListener("keyup", function() {
-
-    const value = this.value.toLowerCase();
+    const value = searchInput.value.toLowerCase();
 
     const filtered = books.filter(book =>
+
         book.title.toLowerCase().includes(value)
+
     );
 
     displayBooks(filtered);
 
 });
+
+/* AJOUT */
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const newBook = {
+
+        title: document.getElementById("title").value,
+
+        author: document.getElementById("author").value,
+
+        year: document.getElementById("year").value,
+
+        price: document.getElementById("price").value,
+
+        image: document.getElementById("image").value
+
+    };
+
+    if(editIndex === -1){
+
+        books.push(newBook);
+
+    } else {
+
+        books[editIndex] = newBook;
+
+        editIndex = -1;
+
+    }
+
+    displayBooks(books);
+
+    form.reset();
+
+});
+
+/* SUPPRESSION */
+
+function deleteBook(index){
+
+    if(confirm("Voulez-vous supprimer ce livre ?")){
+
+        books.splice(index, 1);
+
+        displayBooks(books);
+
+    }
+
+}
+
+/* MODIFICATION */
+
+function editBook(index){
+
+    const book = books[index];
+
+    document.getElementById("title").value = book.title;
+
+    document.getElementById("author").value = book.author;
+
+    document.getElementById("year").value = book.year;
+
+    document.getElementById("price").value = book.price;
+
+    document.getElementById("image").value = book.image;
+
+    editIndex = index;
+
+}
+
+/* VOIR */
+
+function viewBook(index){
+
+    const book = books[index];
+
+    document.getElementById("modalTitle").innerText = book.title;
+
+    document.getElementById("modalAuthor").innerText = book.author;
+
+    document.getElementById("modalYear").innerText = book.year;
+
+    document.getElementById("modalPrice").innerText = book.price + " FCFA";
+
+    document.getElementById("modalImage").src = book.image;
+
+    modal.style.display = "block";
+
+}
+
+/* FERMER MODAL */
+
+closeModal.onclick = function(){
+
+    modal.style.display = "none";
+
+}
+
+window.onclick = function(event){
+
+    if(event.target == modal){
+
+        modal.style.display = "none";
+
+    }
+
+}
